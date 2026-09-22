827c438c53fc52000f01bbcd452f5def0f66eb34
woctordho <woctordho@outlook.com>
2026-09-22 18:13:48 +0800

    Fix MAX_JOBS on Windows in setup.py

--- setup.py
+++ setup.py
@@ -351,9 +351,9 @@ class CMakeBuild(build_ext):
             cmake_args += ["-DCMAKE_MODULE_LINKER_FLAGS_INIT=/Brepro"]
             cmake_args += ["-DCMAKE_SHARED_LINKER_FLAGS_INIT=/Brepro"]
             cmake_args += ["-DCMAKE_STATIC_LINKER_FLAGS_INIT=/Brepro"]
-        else:
-            max_jobs = os.getenv("MAX_JOBS", str(2 * os.cpu_count()))
-            build_args += ['-j' + max_jobs]
+
+        max_jobs = os.getenv("MAX_JOBS", str(2 * os.cpu_count()))
+        build_args += ['-j' + max_jobs]
 
         if check_env_flag("TRITON_BUILD_WITH_CLANG_LLD"):
             cmake_args += [
